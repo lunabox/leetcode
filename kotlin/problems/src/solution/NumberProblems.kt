f742eab6309416b5f7b9f51a186016f85290f36c
maisonwan <maisonwan@gmail.com>
2020-04-04 15:35:17 +0800

    cells-with-odd-values-in-a-matrix

--- kotlin/problems/src/solution/NumberProblems.kt
+++ kotlin/problems/src/solution/NumberProblems.kt
@@ -649,8 +649,6 @@ class NumberProblems {
     fun oddCells(n: Int, m: Int, indices: Array<IntArray>): Int {
         val data = Array(n) { IntArray(m) { 0 } }
         indices.forEach { loc ->
-            val x = loc[0]
-            val y = loc[1]
             repeat(m) {
                 data[loc[0]][it]++
             }
