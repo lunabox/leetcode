d629abc1b5cba4900bfe18c7e87a36f7fab37d15
maisonwan <maisonwan@gmail.com>
2020-04-10 22:46:43 +0800

    update

--- kotlin/problems/src/solution/NumberProblems.kt
+++ kotlin/problems/src/solution/NumberProblems.kt
@@ -809,4 +809,19 @@ class NumberProblems {
         }
         return result.reverse().toString()
     }
+
+    fun rotate(matrix: Array<IntArray>): Unit {
+        var temp = 0
+        val n = matrix.size
+        for (i in 0 until n / 2) {
+            for (j in i until n - i - 1) {
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[n - j - 1][i]
+                matrix[n - j - 1][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = temp
+            }
+        }
+    }
+
 }
