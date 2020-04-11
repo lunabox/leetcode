04bb72ddb846c8569d2f9e19cc6ee26d4188f7e8
maisonwan <maisonwan@gmail.com>
2020-04-11 21:35:47 +0800

    sort-integers-by-the-number-of-1-bits

---
+++ kotlin/problems/src/solution/BitProblems.kt
@@ -0,0 +1,37 @@
+package solution
+
+class BitProblems {
+
+    /**
+     * https://leetcode-cn.com/problems/single-number-iii/
+     */
+    fun singleNumber(nums: IntArray): IntArray {
+        var mask = 0
+        nums.forEach {
+            mask = mask.xor(it)
+        }
+        val last = mask.and(-mask)
+        var x = 0
+        nums.forEach {
+            if (it.and(last) != 0) {
+                x = x.xor(it)
+            }
+        }
+        return intArrayOf(x, mask.xor(x))
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/sort-integers-by-the-number-of-1-bits/
+     */
+    fun sortByBits(arr: IntArray): IntArray {
+        return arr.sortedWith(Comparator<Int> { o1, o2 ->
+            val b1 = Integer.bitCount(o1)
+            val b2 = Integer.bitCount(o2)
+            if (b1 == b2) {
+                o1 - o2
+            } else {
+                b1 - b2
+            }
+        }).toIntArray()
+    }
+}
